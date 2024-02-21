0f50e80422a0cd023eb4f8c4dbd6469a17196a75
JL <j.luis.enriquez77@gmail.com>
2024-02-21 10:51:08 -0600

    Initial Commit

---
+++ FlickrSearchApp/FlickrSearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrSearchAppApp.swift
+//  FlickrSearchApp
+//
+//  Created by JL on 21/02/24.
+//
+
+import SwiftUI
+
+@main
+struct FlickrSearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
